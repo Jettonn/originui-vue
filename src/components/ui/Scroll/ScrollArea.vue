c129867be65ff047c3ce034cee5bc410b78637a5
Rick Tibbe <rick@skrypt.nl>
2024-12-03 17:19:56 +0100

    feat: implement Scroll ui components

---
+++ src/components/ui/Scroll/ScrollArea.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { ScrollAreaRoot, ScrollAreaViewport, ScrollAreaCorner } from 'radix-vue';
+import { cn } from '@/lib/utils';
+import { ScrollBar } from './index'
+
+defineProps({
+  className: {
+    type: String,
+    default: '',
+  },
+});
+
+const rootRef = ref(null);
+</script>
+
+<template>
+  <ScrollAreaRoot ref="rootRef" :class="cn('relative overflow-hidden', className)">
+    <ScrollAreaViewport class="h-full w-full rounded-[inherit]">
+      <slot />
+    </ScrollAreaViewport>
+    <ScrollBar />
+    <ScrollAreaCorner />
+  </ScrollAreaRoot>
+</template>
