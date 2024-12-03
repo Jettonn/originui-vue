703f448c84138c5bdaf9c1aa5233f6b076877121
Rick Tibbe <rick@skrypt.nl>
2024-12-03 17:05:05 +0100

    feat: implement Dialog ui components

---
+++ src/components/ui/Dialog/DialogHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+
+defineProps({
+  className: {
+    type: String,
+    default: '',
+  },
+});
+</script>
+
+<template>
+  <div :class="cn('flex flex-col space-y-1.5 text-center sm:text-left', className)" v-bind="$attrs">
+    <slot />
+  </div>
+</template>
