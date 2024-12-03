1c6bdf9c5bd9c34b910deb6ef19eb4cdea3b7fda
Rick Tibbe <rick@skrypt.nl>
2024-12-03 16:59:16 +0100

    fix: button content overflow

--- src/components/buttons/Button19.vue
+++ src/components/buttons/Button19.vue
@@ -4,7 +4,7 @@ import { Plus } from 'lucide-vue-next'
 </script>
 
 <template>
-  <Button variant="outline" class="aspect-square max-sm:p-0">
+  <Button variant="outline" class="max-sm:aspect-square max-sm:p-0">
     <Plus class="opacity-60 sm:-ms-1 sm:me-2" :size="16" :stroke-width="2" aria-hidden="true" />
     <span class="max-sm:sr-only">Add new</span>
   </Button>
