f01cd59bd701e7767db61e57596bca0de089e021
Rick Tibbe <rick@skrypt.nl>
2024-12-03 16:22:59 +0100

    feat: add Dialog02

---
+++ src/components/dialogs/Dialog02.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/AlertDialog'
+import Button from '@/components/ui/Button.vue'
+import { CircleAlert } from 'lucide-vue-next'
+</script>
+
+<template>
+  <AlertDialog>
+    <AlertDialogTrigger asChild>
+      <Button variant="outline">Alert dialog with icon</Button>
+    </AlertDialogTrigger>
+    <AlertDialogContent>
+      <div class="flex flex-col gap-2 max-sm:items-center sm:flex-row sm:gap-4">
+        <div
+          class="flex size-9 shrink-0 items-center justify-center rounded-full border border-border"
+          aria-hidden="true"
+        >
+          <CircleAlert class="opacity-80" :size="16" :stroke-width="2" aria-hidden="true" />
+        </div>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Are you sure you want to delete your account? All your data will be removed.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+      </div>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogAction>Confirm</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
