8d11826ddbc61e17ebc1a6467887011bb36fa284
Misbah Ansori <misbah.ansori24@gmail.com>
2025-02-18 16:44:05 +0700

    Feat: add initial badge

---
+++ src/components/badges/Badge01.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Badge from '@/components/ui/Badge.vue'
+</script>
+
+<template>
+  <Badge>Badge</Badge>
+</template>
---
+++ src/components/badges/Badge03.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Zap } from 'lucide-vue-next'
+import Badge from '@/components/ui/Badge.vue'
+</script>
+
+<template>
+  <Badge class="gap-1">
+    <Zap class="-ms-0.5 opacity-60" :size="12" :stroke-width="2" aria-hidden="true" />
+    Badge
+  </Badge>
+</template>
